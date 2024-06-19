964420c296d7cb570e1e3fc97dce6c995519cf8e
luizandrenf <luizandrenf@gmail.com>
2024-06-19 16:58:33 -0400

    creation of endpoist for list all transactios and for find transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,8 +4,30 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions')
+      .where('id', id)
+      .first()
+      .select()
+
+    return { transaction }
+  })
+
   app.post('/', async (req, res) => {
-    // {title, amount, type: credit or debit}
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
